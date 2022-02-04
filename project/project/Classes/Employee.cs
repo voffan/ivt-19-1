e10fae0a3f87a31cebe258b6b9610fa72419ea1b
Artur Budikin <artbudikin@gmail.com>
2022-02-04 14:29:16 +0900

    employee class

--- project/project/Classes/Employee.cs
+++ project/project/Classes/Employee.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace project.Classes
 {
     class Employee
     {
+        public int Id { get;set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public Position position { get; set; }
+        public int PhoneNumber { get; set; }
+        [MaxLength(50)]
+        public string Adress { get; set; }
     }
 }
