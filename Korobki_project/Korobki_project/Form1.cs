6508f8a852ded71f978c45750dcb03d40499dea3
abudikin <54790361+abudikin@users.noreply.github.com>
2022-03-03 09:18:34 +0900

    forms

--- Korobki_project/Korobki_project/Form1.cs
+++ Korobki_project/Korobki_project/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Korobki_project;
 
 namespace Korobki_project
 {
@@ -15,7 +16,61 @@ namespace Korobki_project
         public Form1()
         {
             InitializeComponent();
+            Context c = new Context();
+            dataGridView1.DataSource = c.Employees.ToList();
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+            dataGridView1.Columns[6].Visible = false;
+            dataGridView1.Columns[7].Visible = false;
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    Context c0 = new Context();
+                    dataGridView1.DataSource = c0.Employees.ToList();
+                    dataGridView1.Columns[1].Visible = false;
+                    dataGridView1.Columns[2].Visible = false;
+                    dataGridView1.Columns[5].Visible = false;
+                    dataGridView1.Columns[6].Visible = false;
+                    dataGridView1.Columns[7].Visible = false;
+                    break;
+                case 1:
+                    Context c1 = new Context();
+                    dataGridView1.DataSource = c1.Plans.ToList();
+
+                    break;
+                case 2:
+                    Context c2 = new Context();
+                    dataGridView1.DataSource = c2.Productions.ToList();
+
+                    break;
+                case 3:
+                    Context c3 = new Context();
+                    dataGridView1.DataSource = c3.Products.ToList();
+
+                    break;
+                case 4:
+                    Context c4 = new Context();
+                    dataGridView1.DataSource = c4.Shifts.ToList();
+
+                    break;
+                case 5:
+                    Context c5 = new Context();
+                    dataGridView1.DataSource = c5.Schedules.ToList();
+
+                    break;
+                case 6:
+                    Context c6 = new Context();
+                    dataGridView1.DataSource = c6.Typees.ToList();
+
+                    break;
+      
+            }
+            label1.Text = "Текущий столбец: " + listBox1.SelectedItem.ToString();
+        }
     }
 }
