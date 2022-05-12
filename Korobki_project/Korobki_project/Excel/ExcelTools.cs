03b91270f7140736712b99909f7d0588e8652f53
Ayhal <vasilev2001as@gmail.com>
2022-05-12 13:07:01 +0900

    excel

---
+++ Korobki_project/Korobki_project/Excel/ExcelTools.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace Korobki_project.Excel
+{
+	class ExcelTools : IDisposable
+	{
+		private Application _excel;
+		private Workbook _workbook;
+		private string _filePath;
+
+		public ExcelTools()
+		{
+			_excel = new Microsoft.Office.Interop.Excel.Application();
+		}
+
+		internal void Clear()
+		{
+			_excel.Workbooks.Close();
+			_excel.Quit();
+			GC.Collect();
+		}
+
+		internal bool Open(string filePath)
+		{
+			try
+			{
+				if (File.Exists(filePath))
+				{
+					_workbook = _excel.Workbooks.Open(filePath);
+				}
+				else
+				{
+					_workbook = _excel.Workbooks.Add();
+					_filePath = filePath;
+				}
+				return true;
+			}
+			catch (Exception ex) { Console.WriteLine(ex.Message); }
+			return false;
+		}
+
+		internal bool Set(string column, int row, object data)
+		{
+			try
+			{
+				Worksheet _worksheet = (Microsoft.Office.Interop.Excel.Worksheet)_excel.ActiveSheet;
+
+				if (row == 1)
+				{
+					_worksheet.Cells[row, column].Font.Bold = true;
+					_worksheet.Cells[row, column].Font.Size = 14;
+					_worksheet.Cells[row, column].HorizontalAlignment = Microsoft.Office.Interop.Excel.Constants.xlCenter;
+					_worksheet.Cells[row, column].EntireColumn.AutoFit();
+					_worksheet.Cells[row, column].EntireRow.AutoFit();
+				}
+				else
+				{
+					if (data is string)
+					{
+						_worksheet.Cells[row, column].Font.Italic = true;
+						_worksheet.Cells[row, column].HorizontalAlignment = Microsoft.Office.Interop.Excel.Constants.xlLeft;
+					}
+					else
+					{
+						_worksheet.Cells[row, column].Font.Italic = false;
+						_worksheet.Cells[row, column].HorizontalAlignment = Microsoft.Office.Interop.Excel.Constants.xlRight;
+					}
+					_worksheet.Cells[row, column].EntireColumn.AutoFit();
+					_worksheet.Cells[row, column].EntireRow.AutoFit();
+				}
+
+				_worksheet.Cells[row, column] = data;
+
+				return true;
+			}
+			catch (Exception ex) { Console.WriteLine(ex.Message); }
+			return false;
+		}
+
+		internal void Save()
+		{
+			if (!string.IsNullOrEmpty(_filePath))
+			{
+				_workbook.SaveAs(_filePath);
+				_filePath = null;
+			}
+			else
+			{
+				_workbook.Save();
+			}
+		}
+		public void Dispose()
+		{
+			try
+			{
+				_workbook.Close();
+			}
+			catch (Exception ex) { Console.WriteLine(ex.Message); }
+		}
+	}
+}
--- Korobki_project/Korobki_project/UI/MenuForm.Designer.cs
+++ Korobki_project/Korobki_project/UI/MenuForm.Designer.cs
@@ -29,188 +29,200 @@ namespace Korobki_project
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.btnadd = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(47, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Employees";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(128, 27);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Plans";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(209, 27);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Productions";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(298, 27);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Position";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(387, 27);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(83, 23);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Product";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(476, 27);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(83, 23);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "Schedule";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(565, 27);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(83, 23);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "Shift";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(654, 27);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(83, 23);
-            this.button8.TabIndex = 8;
-            this.button8.Text = "Typee";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // btnadd
-            // 
-            this.btnadd.Location = new System.Drawing.Point(651, 403);
-            this.btnadd.Name = "btnadd";
-            this.btnadd.Size = new System.Drawing.Size(86, 28);
-            this.btnadd.TabIndex = 9;
-            this.btnadd.Text = "Добавить";
-            this.btnadd.UseVisualStyleBackColor = true;
-            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(47, 404);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(85, 28);
-            this.button9.TabIndex = 10;
-            this.button9.Text = "Удаление";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(47, 97);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(690, 300);
-            this.dataGridView1.TabIndex = 11;
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(539, 404);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(106, 28);
-            this.button10.TabIndex = 12;
-            this.button10.Text = "Редактировать";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click_1);
-            // 
-            // button11
-            // 
-            this.button11.Location = new System.Drawing.Point(298, 67);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(83, 23);
-            this.button11.TabIndex = 13;
-            this.button11.Text = "Поиск";
-            this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(47, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(245, 23);
-            this.textBox1.TabIndex = 14;
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
-            // 
-            // MenuForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(782, 454);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button11);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.btnadd);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.Name = "MenuForm";
-            this.Text = "MenuForm";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MenuForm_FormClosed);
-            this.Load += new System.EventHandler(this.MenuForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.btnadd = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button12 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(47, 27);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Employees";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(128, 27);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Plans";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(209, 27);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(83, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Productions";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(298, 27);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(83, 23);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "Position";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(387, 27);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(83, 23);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "Product";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(476, 27);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(83, 23);
+			this.button6.TabIndex = 6;
+			this.button6.Text = "Schedule";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(565, 27);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(83, 23);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "Shift";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(654, 27);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(83, 23);
+			this.button8.TabIndex = 8;
+			this.button8.Text = "Typee";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// btnadd
+			// 
+			this.btnadd.Location = new System.Drawing.Point(651, 403);
+			this.btnadd.Name = "btnadd";
+			this.btnadd.Size = new System.Drawing.Size(86, 28);
+			this.btnadd.TabIndex = 9;
+			this.btnadd.Text = "Добавить";
+			this.btnadd.UseVisualStyleBackColor = true;
+			this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(47, 404);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(85, 28);
+			this.button9.TabIndex = 10;
+			this.button9.Text = "Удаление";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(47, 97);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(690, 300);
+			this.dataGridView1.TabIndex = 11;
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(539, 404);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(106, 28);
+			this.button10.TabIndex = 12;
+			this.button10.Text = "Редактировать";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click_1);
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(298, 67);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(83, 23);
+			this.button11.TabIndex = 13;
+			this.button11.Text = "Поиск";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(47, 68);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(245, 23);
+			this.textBox1.TabIndex = 14;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			// 
+			// button12
+			// 
+			this.button12.Location = new System.Drawing.Point(654, 67);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(83, 23);
+			this.button12.TabIndex = 15;
+			this.button12.Text = "Отчёт";
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// MenuForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(782, 454);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.btnadd);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "MenuForm";
+			this.Text = "MenuForm";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MenuForm_FormClosed);
+			this.Load += new System.EventHandler(this.MenuForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -230,5 +242,6 @@ namespace Korobki_project
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.TextBox textBox1;
-    }
+		private System.Windows.Forms.Button button12;
+	}
 }
