ce6eea34956844adb1a7d92d58ab9c2017ea896c
TanYak21 <54880602+TanYak21@users.noreply.github.com>
2022-05-20 15:33:02 +0900

    hard work

--- gallerys/gallerys/Forms/MainForm.Designer.cs
+++ gallerys/gallerys/Forms/MainForm.Designer.cs
@@ -37,6 +37,7 @@ namespace gallerys
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Poiskbutton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -131,11 +132,23 @@ namespace gallerys
             this.Poiskbutton.UseVisualStyleBackColor = true;
             this.Poiskbutton.Click += new System.EventHandler(this.Poiskbutton_Click);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(852, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 71);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Отчет";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1093, 877);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Poiskbutton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -166,6 +179,7 @@ namespace gallerys
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button Poiskbutton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
